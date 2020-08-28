235f878637b68bb9b789447241cf64e0839cc1fe
herbertdubon <hdubon@tecnasa.com>
2020-08-27 22:37:58 -0600

    Se cambio a esp el nombre de las columnas en

--- va_v4.py
+++ va_v4.py
@@ -1,19 +1,19 @@
 
 import pandas as pd
 from funciones import calidad_promedio
-df = pd.read_csv(r'D:\Coding\mlintro\01_cleancode\vinoejemplo\winequality-red.csv', sep=';')
+df = pd.read_csv(r'D:\Coding\mlintro\01_cleancode\vinoejemplo\winequality-red-sp.csv', sep=';')
 df.head()
 #Calculo de medianas de metricas
 mediana_alcohol = df.alcohol.median()
 mediana_ph = df.pH.median()
-mediana_azucarResidual = df.residual_sugar.median()
-mediana_acidoCitrico = df.citric_acid.median()
+mediana_azucarResidual = df.azucar_residual.median()
+mediana_acidoCitrico = df.acido_citrico.median()
 
 #Extraccion de datos de metricas
 alcohol = df.alcohol
 pH = df.pH
-azucarResidual = df.residual_sugar
-acidoCitrico = df.citric_acid
+azucarResidual = df.azucar_residual
+acidoCitrico = df.acido_citrico
 
 #Calidad de Alcohol en el vino
 calidad_promedio(mediana_alcohol, alcohol, 'alcohol')
@@ -22,9 +22,9 @@ calidad_promedio(mediana_alcohol, alcohol, 'alcohol')
 calidad_promedio(mediana_ph, pH, 'pH')
 
 #Calidad de Azucar Residual en el vino
-calidad_promedio(mediana_azucarResidual, azucarResidual, 'residual_sugar')
+calidad_promedio(mediana_azucarResidual, azucarResidual, 'azucar_residual')
 
 #Calidad de Ácido Cítrico en el vino
-calidad_promedio(mediana_acidoCitrico, acidoCitrico, 'citric_acid')
+calidad_promedio(mediana_acidoCitrico, acidoCitrico, 'acido_citrico')
 
 
