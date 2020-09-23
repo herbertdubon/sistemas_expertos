88732a9016b78e655dfe9c74ed378ad7035995c0
herbertdubon <hdubon@tecnasa.com>
2020-09-23 14:36:03 -0600

    Creación de interfaz grafica para calculadora

--- Calculadora/calculadora.py
+++ Calculadora/calculadora.py
@@ -20,22 +20,28 @@ operacion = int(input('Cuál operación desea realizar? '))
 #Calculo de Operaciones
 if operacion == 1:
     resultado = x + y
-    print('El resultado de ', x, ' mas ', y, 'es ', resultado)
+    print('El resultado de {} mas {} es igual a {}'.format(x,y,resultado))
+    
 elif operacion == 2:
     resultado = x - y
-    print('El resultado de ', x, ' menos ', y, 'es ', resultado)
+    print('El resultado de {} menos {} es igual a {}'.format(x,y,resultado))
+    
 elif operacion == 3:
     resultado = x * y
-    print('El resultado de ', x, ' por ', y, 'es ', resultado)
+    print('El resultado de {} multiplicado por {} es igual a {}'.format(x,y,resultado))
+    
 elif operacion == 4 and y!=0:
     resultado = x / y
-    print('El resultado de ', x, ' entre ', y, 'es ', resultado)
+    print('El resultado de {} dividido entre {} es igual a {}'.format(x,y,resultado))
+    
 elif operacion == 5:
-    resultado == x**y
-    print('El resultado de ', x, ' elevado a la ', y, 'es ', resultado)
+    resultado = x**y
+    print('El resultado de {} elevado a la {} es igual a {}'.format(x,y,resultado))
+    
 elif operacion==6 and x>0:
     resultado = math.log10(x)
-    print('El resultado del logaritmo de', x, 'es ', resultado)    
+    print('El resultado del logaritmo de {} es igual a {}'.format(x,resultado))
+    
 else:
     print('Elección no valida, vuelva a internatarlo.')
 
