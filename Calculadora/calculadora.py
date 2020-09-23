6155c2072f09e5bcf59caaae8d231b74e05b4489
herbertdubon <hdubon@tecnasa.com>
2020-09-23 15:26:05 -0600

    Se corrigio para que el menu aparezca primero

--- Calculadora/calculadora.py
+++ Calculadora/calculadora.py
@@ -2,46 +2,86 @@ import pandas as pd
 import numpy as np
 import math
 
-#Ingreso de valores de 'x' y 'y'
-x = float(input('Ingrese el valor de x: '))
-y = float(input('Ingrese el valor de y: '))
 
 #Menu de Operaciones
-print('Seleccione la oepracion que desee realizar: ')
-print('1. Suma')
-print('2. Resta')
-print('3. Multiplicacion')
-print('4. Division')
-print('5. Potencia')
-print('6. Logaritmo (del primer numero [x])')
 
-operacion = int(input('Cuál operación desea realizar? '))
+while 1:
+    print('Seleccione la oepracion que desee realizar: ')
+    print('1. Suma')
+    print('2. Resta')
+    print('3. Multiplicacion')
+    print('4. Division')
+    print('5. Potencia')
+    print('6. Logaritmo')
 
-#Calculo de Operaciones
-if operacion == 1:
-    resultado = x + y
-    print('El resultado de {} mas {} es igual a {}'.format(x,y,resultado))
-    
-elif operacion == 2:
-    resultado = x - y
-    print('El resultado de {} menos {} es igual a {}'.format(x,y,resultado))
-    
-elif operacion == 3:
-    resultado = x * y
-    print('El resultado de {} multiplicado por {} es igual a {}'.format(x,y,resultado))
-    
-elif operacion == 4 and y!=0:
-    resultado = x / y
-    print('El resultado de {} dividido entre {} es igual a {}'.format(x,y,resultado))
-    
-elif operacion == 5:
-    resultado = x**y
-    print('El resultado de {} elevado a la {} es igual a {}'.format(x,y,resultado))
-    
-elif operacion==6 and x>0:
-    resultado = math.log10(x)
-    print('El resultado del logaritmo de {} es igual a {}'.format(x,resultado))
-    
-else:
-    print('Elección no valida, vuelva a internatarlo.')
+    operacion = int(input('\nCuál operación desea realizar?\n'))
+
+
+    #Calculo de Operaciones
+    if operacion == 1:
+        x = float(input('Ingrese el valor de x: '))
+        y = float(input('Ingrese el valor de y: '))
+        resultado = x + y
+        print('El resultado de {} mas {} es igual a {}'.format(x,y,resultado))
+        menu = input('\nDesea realizar otra operación? (s/n)')
+        if menu== "n":
+            break
+        
+    elif operacion == 2:
+        x = float(input('Ingrese el valor de x: '))
+        y = float(input('Ingrese el valor de y: '))
+        resultado = x - y
+        print('El resultado de {} menos {} es igual a {}'.format(x,y,resultado))
+        menu = input('\nDesea realizar otra operación? (s/n)')
+        if menu== "n":
+            break
+        
+    elif operacion == 3:
+        x = float(input('Ingrese el valor de x: '))
+        y = float(input('Ingrese el valor de y: '))
+        resultado = x * y
+        print('El resultado de {} multiplicado por {} es igual a {}'.format(x,y,resultado))
+        menu = input('\nDesea realizar otra operación? (s/n)')
+        if menu== "n":
+            break
+        
+    elif operacion == 4:
+        x = float(input('Ingrese el valor de x: '))
+        y = float(input('Ingrese el valor de y: '))
+        if y==0:
+            print('El valor de "y" no puede ser cero')
+            menu = input('\nDesea realizar otra operación? (s/n)')
+            if menu== "n":
+                break
+        else:
+            resultado = x / y
+            print('El resultado de {} dividido entre {} es igual a {}'.format(x,y,resultado))
+            menu = input('\nDesea realizar otra operación? (s/n)')
+            if menu== "n":
+                break
+        
+    elif operacion == 5:
+        x = float(input('Ingrese el valor de x: '))
+        y = float(input('Ingrese el valor de y: '))
+        resultado = x**y
+        print('El resultado de {} elevado a la {} es igual a {}'.format(x,y,resultado))
+        menu = input('\nDesea realizar otra operación? (s/n)')
+        if menu== "n":
+            break
+        
+    elif operacion==6:
+        x = float(input('Ingrese el valor de x: '))
+        if x <= 0:
+            print('El valor de "x" no puede ser menor o igual a cero')
+            menu = input('\nDesea realizar otra operación? (s/n)')
+            if menu== "n":
+                break
+        else:
+            resultado = math.log10(x)
+            print('El resultado del logaritmo de {} es igual a {}'.format(x,resultado))
+            menu = input('\nDesea realizar otra operación? (s/n)')
+            if menu== "n":
+                break        
+    else:
+        print('Elección no valida, vuelva a internatarlo.')
 
